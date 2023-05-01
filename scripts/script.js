387743ed84108f6ff1914bca8ed8df98c518ebc1
RaelEstate <rhyllneri@gmail.com>
2023-05-01 13:24:02 +0800

    modified (a lot)

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+function successAddCart(){
+    var x = document.getElementById("addcart");
+    x.className = "show";
+    setTimeout(function(){x.className = x.className.replace("show", "");}, 3000);
+    console.log("yawa");
+}
